e46b9519a6672a32dc7eca9e201b56d8e7b1ed39
ivan <lazy530@yandex.ru>
2019-06-21 14:35:45 +0700

    fix auth - not working

--- app/src/main/java/com/example/testltech/LoginActivity.kt
+++ app/src/main/java/com/example/testltech/LoginActivity.kt
@@ -15,7 +15,7 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import java.io.DataOutputStream
+import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.StandardCharsets
@@ -76,7 +76,7 @@ class LoginActivity : AppCompatActivity() {
                 url.openConnection()
                 responseText = url.readText()
 
-            } while (responseText==null&&count<=20)
+            } while (responseText==null&&count<=5)
 
 
         }
@@ -86,13 +86,13 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun signIn(v: View){
-        DoPost().execute(etPhone.text.toString(),etPassword.text.toString())
-
+        val doPost = DoPost()
+        doPost.execute(etPhone.text.toString(),etPassword.text.toString())
 
     }
 
-    fun getAuth(_url: String, phone: String?, password: String?):Array<String?>{
-        var responseText= arrayOfNulls<String>(2)
+    fun getAuth(_url: String, phone: String?, password: String?):String?{
+        var responseText:String? = null
         var count = 0
         if (isNetworkConnected()) {
             val message = "phone=$phone&password=$password"
@@ -100,26 +100,36 @@ class LoginActivity : AppCompatActivity() {
             val con = url.openConnection() as HttpURLConnection
                 with(con){
                     requestMethod = "POST"
-                    connectTimeout = 30000
+                    connectTimeout = 15000
+                    readTimeout = 15000
                     doOutput = true
+                    doInput = true
                     setRequestProperty("Content-Type","application/x-www-form-urlencoded")
+
                 }
+
             val postData: ByteArray = message.toByteArray(StandardCharsets.UTF_8)
             try {
                 val outputStream = DataOutputStream(con.outputStream)
-                outputStream.write(postData)
-                outputStream.flush()
-            } catch (exception: Exception) {}
+                val writer = BufferedWriter(OutputStreamWriter(outputStream))
+                writer.write(message)
+                writer.flush()
+                writer.close()
+                outputStream.close()
 
-            when (con.responseCode){
-                HttpURLConnection.HTTP_OK -> {responseText[0] = HttpURLConnection.HTTP_OK.toString()
-                    responseText[1]=url.readText()}
-                HttpURLConnection.HTTP_BAD_REQUEST -> {responseText[0] = HttpURLConnection.HTTP_BAD_REQUEST.toString()
-                    responseText[1]=url.readText()}
-                HttpURLConnection.HTTP_UNAUTHORIZED -> {responseText[0] = HttpURLConnection.HTTP_UNAUTHORIZED.toString()
-                    responseText[1]=url.readText()}
+                val inputStream = DataInputStream(con.inputStream)
+                val reader = BufferedReader(InputStreamReader(inputStream))
+                responseText = con.responseMessage
+
+                do {
+                    count++
+
+
+            } while (responseText==null&&count<10)
+
+            } catch (exception: Exception) {} finally {
+                con.disconnect()
             }
-
 
 
 
@@ -128,7 +138,6 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun parseResponsePhoneMask(response:String):String{
-        //val responsetext = "{\"phoneMask\":\"+44 ХХХХ-ХХХХХХ\"}"
 
         val gson = Gson()
         val type = object :TypeToken<PhoneMask>(){}.type
@@ -139,7 +148,6 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun parseResponseAuthAnswer(response: String?):Boolean{
-        //val responsetext = "{\"phoneMask\":\"+44 ХХХХ-ХХХХХХ\"}"
 
         val gson = Gson()
         val type = object :TypeToken<AuthAnswer>(){}.type
@@ -166,10 +174,10 @@ class LoginActivity : AppCompatActivity() {
 
     fun updateAuth(auth:Boolean){
         if (auth) {
-            Toast.makeText(this@LoginActivity,"Auth Good!",Toast.LENGTH_LONG)
+            etPhone.setText(auth.toString())
         } else {
-            Toast.makeText(this@LoginActivity,"Auth failed - wrong Phone or Password!",Toast.LENGTH_LONG)
-        }
+            etPhone.setText(auth.toString())
+       }
 
     }
 
@@ -188,25 +196,27 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
-    inner class DoPost  : AsyncTask<String, Unit, Array<String?>>() {
-        override fun doInBackground(vararg params: String?): Array<String?> {
-            var responseText= arrayOfNulls<String>(2)
+    inner class DoPost  : AsyncTask<String, Unit, String?>() {
+        override fun doInBackground(vararg params: String?):String? {
+            var responseText:String? = null
             if (params.size!=0){
-                responseText = getAuth(PHONE_MASK_REQUSET_LINK,params[0],params[1])
+                responseText = getAuth(AUTH_REQUSET_LINK,params[0],params[1])
+
             }
             return responseText
         }
 
-        override fun onPostExecute(result: Array<String?>) {
+        override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            if (result!=null&&result[0]!=null&&result[0]!=null) {
-                when{
-                    result[0]=="400" -> Toast.makeText(this@LoginActivity,"Auth failed - Error $result!",Toast.LENGTH_LONG)
-                    result[0]=="401" -> Toast.makeText(this@LoginActivity,"Auth failed - Error $result!",Toast.LENGTH_LONG)
-                }
-                updateAuth(parseResponseAuthAnswer(result[1]))
+            if (result!=null) {
+
+
             }
+            //updateAuth(parseResponseAuthAnswer(result))
+            etPhone.setText(result)
         }
+
+
 
     }
     data class PhoneMask(val phoneMask:String)
